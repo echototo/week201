7306f39ec25aca9c4a293bd81d7dc6dac68f46ac
Thorez Alexandre <thoreza@b11p20.fil.univ-lille1.fr>
2016-11-18 10:42:52 +0100

    test connection db

---
+++ script.sql
@@ -0,0 +1,2 @@
+create table user( email varchar(100) primary key, lastname varchar(80), firstname varchar(80), pwd varchar(45) );
+insert into user values( "mark.zuckerberg@facebook.com", "Zuckerberg", "Mark", "f4c3b00k");
